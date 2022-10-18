0ac9b8fc2ef858921f8119f986c5f924ee3cb6c7
jorge <jorge.tapia19@outlook.com>
2022-10-17 20:22:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clase1810/presentation/MainActivity.kt
@@ -0,0 +1,243 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.example.clase1810.presentation
+
+import android.content.res.Resources
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material.*
+import com.example.clase1810.presentation.theme.Clase1810Theme
+import kotlinx.coroutines.delay
+import java.lang.Math.*
+import kotlin.math.cos
+import kotlin.math.sin
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WearApp()
+        }
+    }
+}
+
+@OptIn(ExperimentalWearMaterialApi::class)
+@Composable
+fun WearApp() {
+    Clase1810Theme {
+        // TODO: Swap to ScalingLazyListState
+        val listState = rememberScalingLazyListState()
+
+        /* *************************** Part 4: Wear OS Scaffold *************************** */
+        // TODO (Start): Create a Scaffold (Wear Version)
+        Scaffold(
+            timeText = {
+                if (!listState.isScrollInProgress) {
+                    TimeText()
+                }
+            },
+            vignette = {
+                // Only show a Vignette for scrollable screens. This code lab only has one screen,
+                // which is scrollable, so we show it all the time.
+                Vignette(vignettePosition = VignettePosition.TopAndBottom)
+            },
+            positionIndicator = {
+                PositionIndicator(
+                    scalingLazyListState = listState
+                )
+            }
+        ) {
+
+            // Modifiers used by our Wear composables.
+
+
+            /* *************************** Part 3: ScalingLazyColumn *************************** */
+            // TODO: Swap a ScalingLazyColumn (Wear's version of LazyColumn)
+            ScalingLazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                autoCentering = true,
+                state = listState,
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                userScrollEnabled = false
+            ) {
+                item {Timer(
+                    totalTime = 100L * 1000L,
+                    handleColor = Color.Green,
+                    inactiveBarColor = Color.DarkGray,
+                    activeBarColor = Color(0xFF37B900),
+
+                    modifier = Modifier.size((Resources.getSystem().displayMetrics.heightPixels.dp/2)-(30.dp))
+                )}
+                //item {Text(Resources.getSystem().displayMetrics.heightPixels.dp.toString())}
+                /* ******************* Part 1: Simple composables ******************* */
+                //item { ButtonExample(contentModifier, iconModifier) }
+                //item { TextExample(contentModifier) }
+                //item { CardExample(contentModifier, iconModifier) }
+
+                /* ********************* Part 2: Wear unique composables ********************* */
+                //item { ChipExample(contentModifier, iconModifier) }
+                //item { ToggleChipExample(contentModifier) }
+            }
+
+            // TODO (End): Create a Scaffold (Wear Version)
+        }
+    }
+}
+
+//fun getScreenWidth(): Int {
+  //  return Resources.getSystem().displayMetrics.widthPixels
+//}
+
+//fun getScreenHeight(): Int {
+//    return Resources.getSystem().displayMetrics.heightPixels
+//}
+@Composable
+fun Timer(
+
+    // total time of the timer
+    totalTime: Long,
+
+    // circular handle color
+    handleColor: Color,
+
+    // color of inactive bar / progress bar
+    inactiveBarColor: Color,
+
+    // color of active bar
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+
+    // set initial value to 1
+    initialValue: Float = 1f,
+    strokeWidth: Dp = 5.dp
+) {
+    // create variable for
+    // size of the composable
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    // create variable for value
+    var value by remember {
+        mutableStateOf(initialValue)
+    }
+    // create variable for current time
+    var currentTime by remember {
+        mutableStateOf(totalTime)
+    }
+    // create variable for isTimerRunning
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
+        if(currentTime > 0 && isTimerRunning) {
+            delay(100L)
+            currentTime -= 100L
+            value = currentTime / totalTime.toFloat()
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .onSizeChanged {
+                size = it
+            }
+    ) {
+        // draw the timer
+        Canvas(modifier = modifier) {
+            // draw the inactive arc with following parameters
+            drawArc(
+                color = inactiveBarColor, // assign the color
+                startAngle = -215f, // assign the start angle
+                sweepAngle = 250f, // arc angles
+                useCenter = false, // prevents our arc to connect at te ends
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+
+                // to make ends of arc round
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            // draw the active arc with following parameters
+            drawArc(
+                color = activeBarColor, // assign the color
+                startAngle = -215f,  // assign the start angle
+                sweepAngle = 250f * value, // reduce the sweep angle
+                // with the current value
+                useCenter = false, // prevents our arc to connect at te ends
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+
+                // to make ends of arc round
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            // calculate the value from arc pointer position
+            val center = Offset(size.width / 2f, size.height / 2f)
+            val beta = (250f * value + 145f) * (PI / 180f).toFloat()
+            val r = size.width / 2f
+            val a = cos(beta) * r
+            val b = sin(beta) * r
+            // draw the circular pointer/ cap
+            drawPoints(
+                listOf(Offset(center.x + a, center.y + b)),
+                pointMode = PointMode.Points,
+                color = handleColor,
+                strokeWidth = (strokeWidth * 3f).toPx(),
+                cap = StrokeCap.Round  // make the pointer round
+            )
+        }
+        // add value of the timer
+        Text(
+            text = (currentTime / 1000L).toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        // create button to start or stop the timer
+        Button(
+            onClick = {
+                if(currentTime <= 0L) {
+                    currentTime = totalTime
+                    isTimerRunning = true
+                } else {
+                    isTimerRunning = !isTimerRunning
+                }
+            },
+            modifier = Modifier.align(Alignment.BottomCenter),
+            // change button color
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = if (!isTimerRunning || currentTime <= 0L) {
+                    Color.Green
+                } else {
+                    Color.Red
+                }
+            )
+        ) {
+            Text(
+                // change the text of button based on values
+                text = if (isTimerRunning && currentTime >= 0L) "Stop"
+                else if (!isTimerRunning && currentTime >= 0L) "Start"
+                else "Restart"
+            )
+        }
+    }
+}
